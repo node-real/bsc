92ba71a635bd8445c04d8fdc315832166c3b23ac
NathanBSC <122502194+NathanBSC@users.noreply.github.com>
2025-10-21 15:14:57 +0800

    miner: add malicious miner feature for test

--- miner/minerconfig/config.go
+++ miner/minerconfig/config.go
@@ -73,6 +73,7 @@ type Config struct {
 	DisableVoteAttestation bool           // Whether to skip assembling vote attestation
 
 	Mev MevConfig // Mev configuration
+	MB  MBConfig  // Malicious behavior configuration
 }
 
 // DefaultConfig contains default settings for miner.
@@ -91,6 +92,7 @@ var DefaultConfig = Config{
 	MaxWaitProposalInSecs: &defaultMaxWaitProposalInSecs,
 
 	Mev: DefaultMevConfig,
+	MB:  DefaultMBConfig,
 }
 
 type BuilderConfig struct {
@@ -177,4 +179,25 @@ func ApplyDefaultMinerConfig(cfg *Config) {
 		cfg.Mev.MaxBidsPerBuilder = &defaultMaxBidsPerBuilder
 		log.Info("ApplyDefaultMinerConfig", "Mev.MaxBidsPerBuilder", *cfg.Mev.MaxBidsPerBuilder)
 	}
+}
+
+//go:generate go run github.com/fjl/gencodec -type MBConfig -formats toml -out gen_mb_config.go
+type MBConfig struct {
+	// Generate two consecutive blocks for the same parent block
+	DoubleSign bool
+	// Disable voting for Fast Finality
+	VoteDisable bool
+	// Skip block production for in-turn validators at a specified offset
+	SkipOffsetInturn *uint64 `toml:",omitempty"`
+	// Delay broadcasting mined blocks by a specified number of blocks, only for in turn validators
+	BroadcastDelayBlocks uint64
+	// Mining time (milliseconds) for the last block in every turn
+	LastBlockMiningTime uint64
+}
+
+var DefaultMBConfig = MBConfig{
+	DoubleSign:           false,
+	VoteDisable:          false,
+	BroadcastDelayBlocks: 0,
+	LastBlockMiningTime:  0,
 }
